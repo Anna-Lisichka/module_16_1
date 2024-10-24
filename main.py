2f891c8fa7b41397cd68ac87689123c59a851dd5
Anna-Lisichka <an_na22@mail.ru>
2024-10-24 23:01:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+# uvicorn main:app
+app = FastAPI()
+
+
+@app.get("/")
+async def Get_Main_Page() -> dict:
+    return {'message': "Главная страница"}
+
+
+@app.get("/user/admin")
+async def Get_Admin_Page() -> dict:
+    return {'message': f"Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def Get_User_Number(user_id: str) -> dict:
+    return {'message': f"Вы вошли как пользователь № {user_id}"}
+
+
+#  полноценные WEB-запросы:
+@app.get("/user")
+async def Get_User_info(username: str, age: int) -> dict:
+    return {'massage': "Информация о пользователе.", 'Имя': username, 'Возраст': age}
